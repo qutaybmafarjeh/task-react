29defacd2e501c8cdc7dad351044c1f6f3e13ace
unknown <qutaybaraed99@gmail.com>
2026-06-13 08:56:46 +0300

    upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Pizza from './recipes/pizza'
 import Kebab from './recipes/kebab'
 import Potato from './recipes/potato'
@@ -7,9 +7,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Navigate to="/pizza" replace />} />
         <Route path="/pizza" element={<Pizza />} />
         <Route path="/kebab" element={<Kebab />} />
         <Route path="/potato" element={<Potato />} />
+        <Route path="*" element={<Navigate to="/pizza" replace />} />
       </Routes>
     </BrowserRouter>
   )
