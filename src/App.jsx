b8891aedd1d6bfe201bf4406225e41e9a48c0fa1
unknown <qutaybaraed99@gmail.com>
2026-06-13 09:07:45 +0300

    upgrade render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Pizza from './recipes/pizza'
 import Kebab from './recipes/kebab'
 import Potato from './recipes/potato'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Navigate to="/pizza" replace />} />
         <Route path="/pizza" element={<Pizza />} />
@@ -13,6 +13,6 @@ export default function App() {
         <Route path="/potato" element={<Potato />} />
         <Route path="*" element={<Navigate to="/pizza" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
